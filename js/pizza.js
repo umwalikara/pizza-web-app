5434e61235495894a908ec21d65c2c0d895ae498
Umwali Clara <umwaliclara@gmail.com>
2019-08-05 13:51:25 +0200

    add script

--- js/pizza.js
+++ js/pizza.js
@@ -1,38 +1,73 @@
-function pizzaorder(pizzasize, pizzacrust, pizzatopping, pizzaquantity) {
-    this.pizzasize = pizzasize;
-    this.pizzacrust =pizzacrust ;
-    this.pizzatopping=pizzatopping;
-    this.pizzaquantity=pizzaquantity;
-    this.pizzaprice=0;
+var PizzatotalPrice =[];
+function pizzaOrder(pizzaSize, pizzaCrust, pizzaTopping, pizzaQuantity) {
+    this.pizzaSize = pizzaSize;
+    this.pizzaCrust =pizzaCrust ;
+    this.pizzaTopping=pizzaTopping;
+    this.pizzaQuantity=pizzaQuantity;
+    this.pizzaPrice=0;
 
   }
-pizzaorder.prototype.pizzapizza = function() { 
-    if (this.pizzasize==="small"){
-        this.pizzaprice+=1000;
+pizzaOrder.prototype.pizzaPizza = function() { 
+    if (this.pizzaSize==="small"){
+        this.pizzaPrice+=1000;
       }
-    else if(this.pizzasize==="medium"){
-        this.pizzaprice+=1500;
+    else if(this.pizzaSize==="medium"){
+        this.pizzaPrice+=1500;
       }
-    else if(this.pizzasize==="large"){
-        this.pizzaprice+=3000; 
+    else if(this.pizzaSize==="large"){
+        this.pizzaPrice+=3000; 
       }
-    if(this.pizzacrust==="option1"){
-        this.pizzaprice+=500;
+    if(this.pizzaCrust==="option1"){
+        this.pizzaPrice+=500;
       }
-    else if(this.pizzacrust==="option2"){
-        this.pizzaprice+=700;
+    else if(this.pizzaCrust==="option2"){
+        this.pizzaPrice+=700;
       }
-    else if(this.pizzacrust==="option3"){
-        this.pizzaprice+=1500;
+    else if(this.pizzaCrust==="option3"){
+        this.pizzaPrice+=1500;
       }
-    if(this.pizzatopping==="top1"){
-        this.pizzaprice+=1500;
+    if(this.pizzaTopping==="top1"){
+        this.pizzaPrice+=1500;
       }
-    else if(this.pizzatopping==="top2"){
-        this.pizzaprice+=2000;
+    else if(this.pizzaTopping==="top2"){
+        this.pizzaPrice+=2000;
       }
-    else if(this.pizzatopping==="top3"){
-        this.pizzaprice+=4000;
+    else if(this.pizzaTopping==="top3"){
+        this.pizzaPrice+=4000;
       }
-    return this.pizzaprice;
+    return this.pizzaPrice;
 }
+pizzaOrder.prototype.finalCost = function () {
+    var TotalPrice = 0;
+    for (var i = 0; i < PizzatotalPrice.length; i ++) {
+      TotalPrice += PizzatotalPrice[i];
+    }
+    return TotalPrice;
+    }
+    // pizzaOrder.prototype.tatolCharge = function(){
+    //    var checkout =0;
+    //    checkout+=PizzatotalPrice[i];
+    //    return checkout;
+    // }
+    pizzaOrder.prototype.tatolCharge = function(){
+       return this.deliveryCharge;
+    }
+
+    $(document).ready(function() {
+        $("form").submit(function(event) {
+            event.preventDefault();
+          var pizzaSize = $("select#size").val();
+          var pizzaCrust = $("input.cru").val();
+          var pizzaTopping = $("input.topp").val();
+        
+        //    var pizzaNumbers = $("input#pizza-number").val();
+          var pizzaDetails = (pizzaSize + ": " + pizzaCrust + ", " + pizzaTopping);
+          var newPizzaOrder = new Order(pizzaSize, pizzaCrust,pizzaTopping);
+          newPizzaOrder.pizzaPizza();
+          PizzatotalPrice.push(newPizzaOrder.pizzaPrice);
+          $("#pizzaDetails").show();
+          $("#price").text(newPizzaOrder.finalCost());
+          $("#pizzaDetail").append("<p>" + pizzaDetails + "</p>");
+          $("#size,.cru, .topp").val("");
+        });
+    })
